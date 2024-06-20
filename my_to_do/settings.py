0e80c39b3530e6c3a22d93e18c73a570145b2491
Richard Nolan <richienolan23@gmail.com>
2024-06-20 20:22:36 +0000

    setup base app templates

--- my_to_do/settings.py
+++ my_to_do/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bsyyln1h@&4vld1fm-tgqlr7w87enj0_8f9x&yq_g4#(k30o15'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
